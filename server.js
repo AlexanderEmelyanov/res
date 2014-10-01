c87960ecfed0e85995fe78630119d81828c3cc88
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2014-10-01 20:04:04 +0000

    Small design features added

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ var WebSocketServer = require('ws').Server;
 
 var Server = new WebSocketServer({port: 8080});
 
-var greetingMessage = {type: 'system', data: {text: 'Welcome to Real-time Events Server'}};
+var greetingMessage = {type: 'message', data: {text: 'Welcome to Real-time Events Server'}};
 
 Server.on('connection', function(ws) {
     console.log('New connection');
     ws.on('message', function(message, flags, buffer) {
-        console.log(message);
+        try{
+            var event = JSON.parse(message);
+            if (event.type == 'message'){
+                ws.send(JSON.stringify({type: 'message', data: {text: 'You say: "' + event.data.text + '"'}}));
+            }
+        } catch (e){
+            console.log('Message JSON format is broker: ' + e.message);
+        }
     });
     ws.send(JSON.stringify(greetingMessage));
 });
